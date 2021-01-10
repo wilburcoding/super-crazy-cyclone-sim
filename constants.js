694a71e6936bd9206fbf949782ce5720b4fc8c79
wilburcoding <66228004+wilburcoding@users.noreply.github.com>
2021-01-10 11:00:39 -0500

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 const TITLE = "Super Crazy Cyclone Simulator";
-const VERSION_NUMBER = "0.3.9.8";
+const VERSION_NUMBER = "0.3.9.9";
 
 const SAVE_FORMAT = 6;  // Format #6 in use starting in v0.2
 const EARLIEST_COMPATIBLE_FORMAT = 0;
